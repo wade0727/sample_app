1cdbdc34fd0a363a27b36eec93e688d5a9ecdc29
Matt <mdwade@gmail.com>
2018-08-16 18:25:49 -0500

    Fix hello render option

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   def hello
-    render text: "Hello, World!"
+    render plain: "Hello, World!"
   end
+
 end
