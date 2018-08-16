4dcd420f77977cacdee5900b0265f58c86863a07
Matt <mdwade@gmail.com>
2018-08-16 17:53:20 -0500

    Fix hello page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   def hello
-    render :html "Hello, World!"
+    render text: "Hello, World!"
   end
 end
